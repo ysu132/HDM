01481dcff76329a218ecabdcee2adb79071d7671
suyuchen <ysu132@aucklanduni.ac.nz>
2025-02-23 03:10:32 +0000

    Add the new files about punchline and background.

---
+++ Data_mining/background.py
@@ -0,0 +1,39 @@
+import os
+import re
+import torch
+import argparse
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+
+model_name_or_path = "wxjiao/alpaca-7b"
+it_src = "You can tune a guitar, but you can't tuna fish."
+
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float16, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=False)
+tokenizer.padding_side = "left"
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+gen_config = GenerationConfig(
+                        temperature=0.2,
+                        do_sample=True,
+                        num_beams=1,
+                        max_new_tokens=256,
+                        eos_token_id=tokenizer.eos_token_id,
+                        pad_token=tokenizer.pad_token_id,
+            )
+s = f"The topic is the news item that the joke is based on. Use a few words to describe the topics of the following input sentence.\n" + \
+                    f"Input: {it_src}\n" + "Topics:"
+
+tokenized = tokenizer(s, padding=True, return_tensors="pt")
+print(tokenized)
+input_ids = tokenized.input_ids.cuda()
+attn_mask = tokenized.attention_mask.cuda()
+input_ids = input_ids[:, :-1] if input_ids[0, -1] == tokenizer.eos_token_id else input_ids
+attn_mask = attn_mask[:, :-1] if input_ids[0, -1] == tokenizer.eos_token_id else attn_mask
+
+generated_ids = model.generate(inputs=input_ids, attention_mask=attn_mask, generation_config=gen_config)
+print(generated_ids)
+original_text = tokenizer.decode(input_ids[0], skip_special_tokens=True)
+gen_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+new_text = gen_text.replace(original_text, "").replace("\n", "").strip()
+print(new_text, flush=True)
